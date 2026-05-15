99063ea4117ba1b0c90af5a55ad5d825fef30bb4
vanshpatel-017 <patelvanshj.17@gmail.com>
2026-05-15 23:42:24 +0530

    Add real-time PPE detection inference pipeline

---
+++ src/inference/video_inference.py
@@ -0,0 +1,70 @@
+import cv2
+
+from ultralytics import YOLO
+
+from src.utils.constants import (
+    TRAINED_MODEL,
+    INPUT_VIDEO,
+    OUTPUT_VIDEO_DIR
+)
+
+from src.utils.video_utils import (
+    get_video_info,
+    create_video_writer
+)
+
+# ============================================================
+# LOAD MODEL
+# ============================================================
+
+model = YOLO(str(TRAINED_MODEL))
+
+# ============================================================
+# OUTPUT VIDEO
+# ============================================================
+
+OUTPUT_VIDEO = (
+    OUTPUT_VIDEO_DIR /
+    "01_detection_demo.mp4"
+)
+
+# ============================================================
+# VIDEO INFO
+# ============================================================
+
+width, height, fps = get_video_info(INPUT_VIDEO)
+
+cap = cv2.VideoCapture(str(INPUT_VIDEO))
+
+writer = create_video_writer(
+    OUTPUT_VIDEO,
+    width,
+    height,
+    fps
+)
+
+# ============================================================
+# INFERENCE LOOP
+# ============================================================
+
+while cap.isOpened():
+
+    success, frame = cap.read()
+
+    if not success:
+        break
+
+    results = model.predict(
+        source=frame,
+        conf=0.35,
+        verbose=False
+    )
+
+    plotted = results[0].plot()
+
+    writer.write(plotted)
+
+cap.release()
+writer.release()
+
+print("Inference complete.")
